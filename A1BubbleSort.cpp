1cceaedebb8564a6ba1a2831c43eedbb64a5b20d
shivani-code345 <shivanikurmi917@gmail.com>
2024-12-08 14:27:10 +0530

    Add files via upload

---
+++ A1BubbleSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={6,7,3,9,0};
+    for(int i=1;i<5;i++){
+        for(int j=0;j<5-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
---
+++ A2InsertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[6]={5,8,2,9,1,0};
+    for(int i=1;i<6;i++){
+        int temp=arr[i];
+        int j=i-1;
+        for(; j>=0;j--){
+            if(arr[j]>temp){
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
